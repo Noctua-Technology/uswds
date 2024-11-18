0f35a523641f97bdc2598436e5b59893d303c395
deebloo <deebloo@proton.me>
2024-11-18 16:58:21 -0500

    add generator

---
+++ plopfile.js
@@ -0,0 +1,32 @@
+/**
+ * @param {import('plop').NodePlopAPI} plop
+ */
+export default function (plop) {
+  plop.setGenerator("element", {
+    description: "adds a new element",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "name of the element",
+        filter(value) {
+          const val = String(value);
+
+          return val.replace("usa-", "");
+        },
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/lib/{{kebabCase name}}/{{kebabCase name}}.element.ts",
+        templateFile: "generators/element/element.ts.hbs",
+      },
+      {
+        type: "add",
+        path: "src/lib/{{kebabCase name}}/{{kebabCase name}}.stories.ts",
+        templateFile: "generators/element/stories.ts.hbs",
+      },
+    ],
+  });
+}
