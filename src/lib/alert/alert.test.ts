2a931d56d2a193e9472065807e3e23fa7b043498
deebloo <deebloo@proton.me>
2024-11-21 13:00:34 -0500

    add accessibility tests for alert

---
+++ src/lib/alert/alert.test.ts
@@ -0,0 +1,23 @@
+import "./alert.element.js";
+
+import { fixture, html, assert } from "@open-wc/testing";
+
+import { USA_ALERT_TYPES } from "./alert-types.js";
+
+describe("usa-alert", () => {
+  for (let alert of USA_ALERT_TYPES) {
+    it(`should be accessible: ${alert}`, async () => {
+      const form = await fixture<HTMLFormElement>(html`
+        <usa-alert type=${alert}>
+          <h3 slot="heading">Status</h3>
+
+          Lorem ipsum dolor sit amet,&nbsp;
+          <usa-link href="#">consectetur adipiscing</usa-link>&nbsp;elit, sed do
+          eiusmod.
+        </usa-alert>
+      `);
+
+      return assert.isAccessible(form);
+    });
+  }
+});
