48bc21927fd73945f3c25f7351803764703c7139
deebloo <deebloo@proton.me>
2024-11-26 16:29:57 -0500

    better ICON story

--- src/lib/icon/icon.stories.ts
+++ src/lib/icon/icon.stories.ts
@@ -9,7 +9,7 @@ import { ICON_TYPES } from "./icon-types.js";
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
   title: "usa-icon",
-  tags: ["autodocs"],
+  tags: [],
   render(args) {
     return html`<usa-icon icon="${args.icon}"></usa-icon>`;
   },
@@ -29,15 +29,32 @@ export default meta;
 
 type Story = StoryObj<USAIconElement>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Example: Story = {
-  args: {},
-};
-
 export const AllIcons: Story = {
   render() {
-    return html`${ICON_TYPES.map(
-      (icon) => html`<usa-icon icon=${icon}></usa-icon>${"\n"}`
-    )}`;
+    async function copyIcon(icon: string) {
+      try {
+        await navigator.clipboard.writeText(
+          `<usa-icon icon=${icon}></usa-icon>`
+        );
+        alert(`copied markup for ${icon}`);
+      } catch (err) {
+        console.error("Failed to copy: ", err);
+      }
+    }
+
+    return html`<div style="display: flex; flex-wrap: wrap; gap: .75rem;">
+      ${html`${ICON_TYPES.map(
+        (icon) => html`
+          <button
+            @click=${() => copyIcon(icon)}
+            style="padding: .5rem 1rem .5rem .5rem; cursor: pointer; background: #fff; border-radius: 4px; gap: .5rem; display: flex; align-items: center"
+          >
+            <usa-icon icon=${icon}></usa-icon>
+
+            ${icon}
+          </button>
+        `
+      )}`}
+    </div>`;
   },
 };
