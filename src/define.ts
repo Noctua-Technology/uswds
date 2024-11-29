a40856ff046fa01ebfd3c361090cead43b2530ad
deebloo <deebloo@proton.me>
2024-11-29 12:13:38 -0500

    update package exprts and definitions

--- src/define.ts
+++ src/define.ts
@@ -1,4 +1,12 @@
 import "./lib/alert/alert.element.js";
+import "./lib/button/button.element.js";
+import "./lib/checkbox/checkbox.element.js";
+import "./lib/description/description.element.js";
 import "./lib/icon/icon.element.js";
 import "./lib/input/input.element.js";
+import "./lib/input-mask/input-mask.element.js";
 import "./lib/link/link.element.js";
+import "./lib/radio/radio.element.js";
+import "./lib/radio/radio-option.element.js";
+import "./lib/select/select-option.element.js";
+import "./lib/tag/tag.element.js";
--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,13 @@
 export { USAAlertElement } from "./lib/alert/alert.element.js";
+export { USAButtonElement } from "./lib/button/button.element.js";
+export { USACheckboxElement } from "./lib/checkbox/checkbox.element.js";
+export { USADescriptionElement } from "./lib/description/description.element.js";
 export { USAIconElement } from "./lib/icon/icon.element.js";
 export { USATextInputElement } from "./lib/input/input.element.js";
+export { USAInputMaskElement } from "./lib/input-mask/input-mask.element.js";
 export { USALinkElement } from "./lib/link/link.element.js";
+export { USARadioElement } from "./lib/radio/radio.element.js";
+export { USARadioOptionElement } from "./lib/radio/radio-option.element.js";
+export { USASelectElement } from "./lib/select/select.element.js";
+export { USASelecOptionElement } from "./lib/select/select-option.element.js";
+export { USATagElement } from "./lib/tag/tag.element.js";
