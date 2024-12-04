720c4ec2974af230d5adfa58eedadcb832116f76
deebloo <deebloo@proton.me>
2024-12-03 22:53:29 -0500

    publish minified bundle

---
+++ rollup-config.js
@@ -0,0 +1,19 @@
+import terser from "@rollup/plugin-terser";
+import { minifyHTMLLiterals } from "minify-html-literals";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "target/lib/define.js",
+  output: {
+    file: "public/uswds.min.js",
+    format: "iife",
+  },
+  plugins: [
+    nodeResolve(),
+    {
+      name: "minify-html-literals",
+      transform: (code) => minifyHTMLLiterals(code)?.code,
+    },
+    terser(),
+  ],
+};
