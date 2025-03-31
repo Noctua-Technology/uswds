a46f9090df86302caf7bec05f619c8117d2d982e
deebloo <deebloo@proton.me>
2025-03-31 15:30:16 -0400

    add more tests for range slider

--- src/lib/range-slider/range-slider.test.ts
+++ src/lib/range-slider/range-slider.test.ts
@@ -1,6 +1,7 @@
 import "./range-slider.element.js";
 
 import { assert, fixture, html } from "@open-wc/testing";
+import { userEvent } from "@testing-library/user-event";
 
 import type { USARangeSliderElement } from "./range-slider.element.js";
 
@@ -11,5 +12,41 @@ describe("usa-range-slider", () => {
     `);
 
     return assert.isAccessible(rangeSlider);
+  });
+
+  it("should submit form with default values", async () => {
+    const form = await fixture<HTMLFormElement>(html`
+      <form>
+        <usa-range-slider name="slider" value="50">Hello World</usa-range-slider>
+
+        <button>Submit</button>
+      </form>
+    `);
+
+    const value = new FormData(form);
+
+    assert.equal(value.get("slider"), "50");
+  });
+
+  it("should update form value as input value changed", async () => {
+    const form = await fixture<HTMLFormElement>(html`
+        <form>
+          <usa-range-slider name="slider" value="50">Hello World</usa-range-slider>
+  
+          <button>Submit</button>
+        </form>
+      `);
+
+    const input = form.querySelector("usa-input");
+    const nativeInput = input?.shadowRoot?.querySelector("input");
+
+    if (nativeInput) {
+      nativeInput.value = "75";
+      nativeInput.dispatchEvent(new Event("change"));
+    }
+
+    const value = new FormData(form);
+
+    assert.equal(value.get("slider"), "50");
   });
 });
