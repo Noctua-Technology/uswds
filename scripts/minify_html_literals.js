87383234774953e0cea0d5fe94debae44c206723
deebloo <deebloo@proton.me>
2025-04-07 11:14:01 -0400

    cleanup

--- scripts/minify_html_literals.js
+++ scripts/minify_html_literals.js
@@ -2,15 +2,15 @@ import { readFile, readdir, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 import { minifyHTMLLiterals } from "minify-html-literals";
 
-async function findJsFiles(dir, files = []) {
+async function findFiles(dir, ext, files = []) {
   const entries = await readdir(dir, { withFileTypes: true });
 
   for (const entry of entries) {
     const fullPath = join(dir, entry.name);
 
     if (entry.isDirectory()) {
-      await findJsFiles(fullPath);
-    } else if (entry.name.endsWith(".js")) {
+      await findFiles(fullPath);
+    } else if (entry.name.endsWith(ext)) {
       const content = await readFile(fullPath, "utf-8");
 
       files.push({ path: fullPath, content });
@@ -20,7 +20,7 @@ async function findJsFiles(dir, files = []) {
   return files;
 }
 
-const files = await findJsFiles("target");
+const files = await findFiles("target", ".js");
 
 for (const file of files) {
   const minified = await minifyHTMLLiterals(file.content, {
