a030844daca75b234d8b5ab9c5982fe37ad17415
deebloo <deebloo@proton.me>
2025-04-14 11:55:02 -0400

    fix accessibility tests

--- src/lib/breadcrumbs/breadcrumbs.test.ts
+++ src/lib/breadcrumbs/breadcrumbs.test.ts
@@ -1,4 +1,5 @@
 import "./breadcrumbs.element.js";
+import "./breadcrumb/breadcrumb.element.js";
 
 import { assert, fixture, html } from "@open-wc/testing";
 
@@ -7,7 +8,12 @@ import type { USABreadcrumbsElement } from "./breadcrumbs.element.js";
 describe("usa-breadcrumbs", () => {
   it("should be accessible", async () => {
     const breadcrumbs = await fixture<USABreadcrumbsElement>(html`
-      <usa-breadcrumbs>Hello World</usa-breadcrumbs>
+      <usa-breadcrumbs>
+        <usa-breadcrumb href="/">Home</usa-breadcrumb>
+        <usa-breadcrumb href="/">Federal Contracting</usa-breadcrumb>
+        <usa-breadcrumb href="/">Contracting assistance programs</usa-breadcrumb>
+        <usa-breadcrumb>Economically disadvantaged women-owned small business federal contracting program</usa-breadcrumb>
+      </usa-breadcrumbs>
     `);
 
     return assert.isAccessible(breadcrumbs);
