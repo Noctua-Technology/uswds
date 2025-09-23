6b27ed0d0e727b947108a1c4b2e48738702a9232
deebloo <deebloo@proton.me>
2025-09-23 11:56:08 -0400

    remove puppeteer

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,16 +1,10 @@
 import { importMapsPlugin } from '@web/dev-server-import-maps';
-import { puppeteerLauncher } from '@web/test-runner-puppeteer';
 
 export default {
   nodeResolve: {
     exportConditions: ['production'],
   },
   files: ['target/**/*.test.js'],
-  browsers: [
-    puppeteerLauncher({
-      args: ['--no-sandbox'],
-    }),
-  ],
   plugins: [
     importMapsPlugin({
       inject: {
