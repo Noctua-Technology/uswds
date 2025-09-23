edb9d49810256bdb2685f004ae6a6276f0f2a8f1
deebloo <deebloo@proton.me>
2025-09-23 10:40:57 -0400

    puppeteer

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,17 +1,23 @@
-import { importMapsPlugin } from "@web/dev-server-import-maps";
+import { importMapsPlugin } from '@web/dev-server-import-maps';
+import { puppeteerLauncher } from '@web/test-runner-puppeteer';
 
 export default {
   nodeResolve: {
-    exportConditions: ["production"],
+    exportConditions: ['production'],
   },
-  files: ["target/**/*.test.js"],
+  files: ['target/**/*.test.js'],
+  browsers: [
+    puppeteerLauncher({
+      args: ['--no-sandbox'],
+    }),
+  ],
   plugins: [
     importMapsPlugin({
       inject: {
         importMap: {
           imports: {
-            "@testing-library/dom": "./testing-library/dom.js",
-            "@testing-library/user-event": "./testing-library/user-event.js",
+            '@testing-library/dom': './testing-library/dom.js',
+            '@testing-library/user-event': './testing-library/user-event.js',
           },
         },
       },
